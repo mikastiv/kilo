09057dfe7589defb31c14b15ad1b4b9cfa92a5b0
mikastiv <mikastiv@outlook.com>
2025-08-24 18:23:27 -0300

    raw mode

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "zig flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    zls.url = "github:zigtools/zls";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, zls, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            zls.packages.${system}.zls
+          ];
+        };
+      });
+}
+
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,31 @@
 const std = @import("std");
+const posix = std.posix;
+
+var stdin_buffer: [1024]u8 = undefined;
+var stdin_reader = std.fs.File.stdin().reader(&stdin_buffer);
+const stdin = &stdin_reader.interface;
+
+var original_termios: posix.termios = undefined;
 
 pub fn main() !void {
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    try enableRawMode();
+    defer disableRawMode() catch {};
+
+    while (true) {
+        const char = stdin.takeByte() catch break;
+        if (char == 'q') break;
+        std.debug.print("{c}", .{char});
+    }
+}
+
+fn enableRawMode() !void {
+    original_termios = try posix.tcgetattr(posix.STDIN_FILENO);
+
+    var raw = original_termios;
+    raw.lflag.ECHO = false;
+    try posix.tcsetattr(posix.STDIN_FILENO, .FLUSH, raw);
+}
+
+fn disableRawMode() !void {
+    try posix.tcsetattr(posix.STDIN_FILENO, .FLUSH, original_termios);
 }
